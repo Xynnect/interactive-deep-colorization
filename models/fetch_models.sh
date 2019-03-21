744b30a42f1b3ab5f297e24782b28f9c683d2499
Dian Yordanov <dido.jordanov7@gmail.com>
2019-03-22 00:42:03 +0200

    Update fetch_models.sh

--- models/fetch_models.sh
+++ models/fetch_models.sh
@@ -1,5 +1,5 @@
 
-wget http://colorization.eecs.berkeley.edu/siggraph/models/model.caffemodel -O interactive-deep-colorization/models/reference_model/model.caffemodel
-wget http://colorization.eecs.berkeley.edu/siggraph/models/global_model.caffemodel -O interactive-deep-colorization/models/global_model/global_model.caffemodel
-wget http://colorization.eecs.berkeley.edu/siggraph/models/dummy.caffemodel -O interactive-deep-colorization/models/global_model/dummy.caffemodel
-wget http://colorization.eecs.berkeley.edu/siggraph/models/pytorch.pth -O interactive-deep-colorization/models/pytorch/model.pth
+wget http://colorization.eecs.berkeley.edu/siggraph/models/model.caffemodel -O /content/interactive-deep-colorization/models/reference_model/model.caffemodel
+wget http://colorization.eecs.berkeley.edu/siggraph/models/global_model.caffemodel -O /content/interactive-deep-colorization/models/global_model/global_model.caffemodel
+wget http://colorization.eecs.berkeley.edu/siggraph/models/dummy.caffemodel -O /content/interactive-deep-colorization/models/global_model/dummy.caffemodel
+wget http://colorization.eecs.berkeley.edu/siggraph/models/pytorch.pth -O /content/interactive-deep-colorization/models/pytorch/model.pth

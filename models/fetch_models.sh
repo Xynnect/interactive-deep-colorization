c839bddaa55587873af82fc67d5f5841ff5002e3
Dian Yordanov <dido.jordanov7@gmail.com>
2019-03-21 22:46:33 +0200

    Update fetch_models.sh

--- models/fetch_models.sh
+++ models/fetch_models.sh
@@ -1,6 +1,5 @@
 
-wget http://colorization.eecs.berkeley.edu/siggraph/models/model.caffemodel -O ./models/reference_model/model.caffemodel
-wget http://colorization.eecs.berkeley.edu/siggraph/models/global_model.caffemodel -O ./models/global_model/global_model.caffemodel
-wget http://colorization.eecs.berkeley.edu/siggraph/models/dummy.caffemodel -O ./models/global_model/dummy.caffemodel
-wget http://colorization.eecs.berkeley.edu/siggraph/models/pytorch.pth -O ./models/pytorch/model.pth
-
+wget http://colorization.eecs.berkeley.edu/siggraph/models/model.caffemodel -O ideepcolor/models/reference_model/model.caffemodel
+wget http://colorization.eecs.berkeley.edu/siggraph/models/global_model.caffemodel -O ideepcolor/models/global_model/global_model.caffemodel
+wget http://colorization.eecs.berkeley.edu/siggraph/models/dummy.caffemodel -O ideepcolor/models/global_model/dummy.caffemodel
+wget http://colorization.eecs.berkeley.edu/siggraph/models/pytorch.pth -O ideepcolor/models/pytorch/model.pth
